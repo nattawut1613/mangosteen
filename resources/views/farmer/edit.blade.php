26acb48359aec389e72f4330c4fdd9b6df5e3e26
nattawut <nattawut1613@gmail.com>
2020-02-21 01:45:29 +0700

    1

---
+++ resources/views/farmer/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+<div class="col-md-12">
+<br />
+
+<h3>Edit Record     </h3>
+<br />
+<form method="post" action="{{action"
+<div>
+<div>
+@endsection
